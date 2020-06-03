8a4131a0655d3b6f7fc92d3200ce8f02cd338604
kirancv4 <kiran.cv@qxf2.com>
2020-06-03 16:50:45 +0530

    Added code to display the Available device templates and based on user choice getting the path of the device templates ...

--- script.py
+++ script.py
@@ -1,69 +1,90 @@
 '''
 This python script is used to print the available templates
+Get the user input for available device templates.
 
 '''
 
 import os
 import argparse
 from colored import fg, bg, attr
+import fnmatch,itertools
+#from faker import Faker
 
 my_dir = ".//data"
 
-def print_templates():
-    #Print the templates
-    print("************Available device templates to use in data directory********")
-
-    for root_dir_path, sub_dirs, files in os.walk(my_dir):
-        
-        color = fg('orchid') + attr('bold')
-        reset = attr('reset')
-                
-        if files:
-            tag = os.path.relpath(root_dir_path, my_dir)
-
-            for file in files:
-                tag_parent = os.path.dirname(tag)
-                sub_folder = os.path.basename(tag)
-
-                print(sub_folder if sub_folder else "","--->",(color+(file)+reset))
-        
-    print("************End********")
-
-def print_arguments():
-    #This function is used to print the templates chosen by the user
-
-    # Initialize parser 
-    parser = argparse.ArgumentParser() 
-  
-    # Adding optional argument 
-    parser.add_argument("-d", "--Output", nargs='+' , help = "Show Output") 
-    parser.add_argument("-p", "--Print",required=False,help = "enter 'display' to show the available templates" )
-    # Read arguments from command line 
-    args = parser.parse_args() 
-  
-    if args.Output: 
-          #print(args.Output)
-          for i in args.Output:
-            try: 
-              os.system("snmpsimd.py --v3-engine-id=010203040505060880 --v3-user=qxf2 --data-dir=./data/os/ubuntu --agent-udpv4-endpoint=127.0.0.1:6464 &")           
-            except OSError:
-                raise ValueError("error in running the snmp device 'snmpsimd.py' command")
-            #snmpsimd.py --v3-engine-id=010203040505060809 --v3-user=qxf2 --v3-auth-key=testqxf2 --agent-udpv4-endpoint=127.0.0.1:6464 --data-dir=/home/ubuntu/data/
-
-
-    elif args.Print.upper() == 'DISPLAY':
-        print_templates()
-
+class simulate_snmp_devices():
+    'Class to simulate snmp devices' 
 
     
 
+    def available_templates(self,*args):
+        #Print the templates
+        
+        #if args == 'True':
+            print("************Available device templates to use in data directory********")
 
+            for root_dir_path, sub_dirs, files in os.walk(my_dir):
+                
+                color = fg('orchid') + attr('bold')
+                reset = attr('reset')
+                        
+                if files:
+                    tag = os.path.relpath(root_dir_path, my_dir)
 
+                    for file in files:
+                        tag_parent = os.path.dirname(tag)
+                        sub_folder = os.path.basename(tag)
+
+                        print(sub_folder if sub_folder else "","--->",(color+(file)+reset))
+                
+            print("************End********")
+        
+
+    def parse_args(self):
+        #This function is used to print the templates chosen by the user
+
+        # Initialize parser 
+        parser = argparse.ArgumentParser() 
+    
+        # Adding optional argument 
+        parser.add_argument('-d',"--devices",required=False,nargs='+',help = "Enter the file names ",action="count",default=False) 
+        parser.add_argument("-p", "--Print",required=False,action="store_true",help = " type '-p' and hit 'Enter' to show available device templates." )
+        
+        # Read arguments from command line 
+        
+        args = parser.parse_args() 
+
+        return args        
+
+    def find_dev_template(self,*args):
+        #To find the device template path in data directory.
+        my_dir = ".//data"
+        dev_templates = list(itertools.chain(*args)) 
+        for root_dir_path, sub_dirs, files in os.walk(my_dir):
+            
+                if files:
+                    tag = os.path.relpath(root_dir_path, my_dir)
+                    for device in dev_templates:
+                        for file in files:                    
+                            tag_parent = os.path.dirname(tag)
+                            sub_folder = os.path.basename(tag)
+                            if fnmatch.fnmatch(file,device):
+                                  print(os.path.normpath(os.path.join(my_dir,tag_parent,sub_folder)))
+        
+    
+    
+            
     
 
 
 
 if __name__ == "__main__":
-    #print_templates()
-    print_arguments()
+    devices = simulate_snmp_devices()
+    args = devices.parse_args()    
+    if args.Print is True:
+        devices.available_templates(args.Print)
+    elif args.devices is not False:
+        devices.find_dev_template(args.devices)
+    
+    
     
--- test.py
+++ test.py
@@ -1,13 +1,17 @@
-import argparse 
+import argparse,os,fnmatch
+my_dir= ".//data"
+device=['xp.snmprec','ubuntu.snmprec']
+for root_dir_path, sub_dirs, files in os.walk(my_dir):
 
-# Initialize parser 
-parser = argparse.ArgumentParser() 
-  
-# Adding optional argument 
-parser.add_argument("-d", "--Output", nargs='+' , help = "Enter the template/templates") 
-  
-# Read arguments from command line 
-args = parser.parse_args() 
-  
-if args.Output: 
-    print("Diplaying Output as: % s" % args.Output) 
+        if files:
+            tag = os.path.relpath(root_dir_path, my_dir)
+            for dev in device:
+                for file in files:
+                    tag_parent = os.path.dirname(tag)
+                    sub_folder = os.path.basename(tag)
+                    if fnmatch.fnmatch(file,dev):
+                        print(os.path.normpath(os.path.join(my_dir,tag_parent,sub_folder)))
+                #print(sub_folder if sub_folder else "","--->",(file))
+                #print (tag_parent)
+                #print( "File:",file,"belongs in",tag_parent, sub_folder if sub_folder else "")
+                    
