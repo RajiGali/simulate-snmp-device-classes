8fd0a2657a754e2a581a94ed6b7c127055e5429f
Kiran CV <52411326+kirancv4@users.noreply.github.com>
2020-06-05 09:33:12 +0530

    Update script.py
    "Resolved the error in --logging-method option by adding criterion to it and made changes in -help in '-d'...

--- script.py
+++ script.py
@@ -1,12 +1,5 @@
-'''
-This python script is used to print the available templates
-Get the user input for available device templates.
-
-'''
-
 import os
 import argparse
-from colored import fg, bg, attr
 import fnmatch,itertools
 from faker import Faker
 import socket
@@ -14,21 +7,18 @@ import socket
 my_dir = ".//data"
 
 class simulate_snmp_devices():
-    'Class to simulate snmp devices' 
+    'Class to simulate snmp devices'
 
-    
+
 
     def available_templates(self,*args):
         #Print the templates
-        
+
         #if args == 'True':
             print("************Available device templates to use in data directory********")
 
             for root_dir_path, sub_dirs, files in os.walk(my_dir):
-                
-                color = fg('orchid') + attr('bold')
-                reset = attr('reset')
-                        
+
                 if files:
                     tag = os.path.relpath(root_dir_path, my_dir)
 
@@ -36,26 +26,28 @@ class simulate_snmp_devices():
                         tag_parent = os.path.dirname(tag)
                         sub_folder = os.path.basename(tag)
 
-                        print(sub_folder if sub_folder else "","--->",(color+(file)+reset))
-                
+                        print(sub_folder if sub_folder else "","--->",(file))
+
             print("************End********")
-        
+
 
     def parse_args(self):
         #This function is used to print the templates chosen by the user
 
-        # Initialize parser 
-        parser = argparse.ArgumentParser() 
-    
-        # Adding optional argument 
-        parser.add_argument('-d',"--devices",required=False,nargs='+',help = "Enter the file names ",default=False) 
-        parser.add_argument("-p", "--Print",required=False,action="store_true",help = " type '-p' and hit 'Enter' to show available device templates." )
-        
-        # Read arguments from command line 
-        
-        args = parser.parse_args() 
+        # Initialize parser
+        parser = argparse.ArgumentParser(description='SNMP Simulator.')
 
-        return args        
+        # Adding optional argument
+        parser.add_argument('-d',"--devices",required=False,nargs='+',help = "Enter the file names using '-d' option Ex:-      -d xp.snmprec ubuntu.snmprec  ... " ,default=False)
+        parser.add_argument("-p", "--Print",required=False,action="store_true",help = " type '-p' and hit 'Enter' to :show available device templates to use..." )
+
+
+
+        # Read arguments from command line
+
+        args = parser.parse_args()
+
+        return args
 
     def find_dev_template(self,*args):
         #To find the device template path in data directory.
@@ -63,18 +55,18 @@ class simulate_snmp_devices():
         dev_templates = list(itertools.chain(*args))
         template_path=[]
         for root_dir_path, sub_dirs, files in os.walk(my_dir):
-            
+
                 if files:
                     tag = os.path.relpath(root_dir_path, my_dir)
                     for device in dev_templates:
-                        for file in files:                    
+                        for file in files:
                             tag_parent = os.path.dirname(tag)
                             sub_folder = os.path.basename(tag)
                             if fnmatch.fnmatch(file,device):
                                   template_path.append(os.path.normpath(os.path.join(my_dir,tag_parent,sub_folder)))
-                                
-        return template_path    
-    
+
+        return template_path
+
     def get_open_port(self,host):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -87,36 +79,38 @@ class simulate_snmp_devices():
     def create_snmp(self,*args):
         path=list(itertools.chain(*args))
         num_devices = len(path)
-        
+
         faker = Faker()
         port_list = []
         for i in range(num_devices):
             try:
                 port = self.get_open_port(host="127.0.0.1")
                 port_list.append(port)
-                print(num_devices,path[i])
-                #os.system("echo 'em7admin'|snmpsimd.py --data-dir=./data --agent-udpv4-endpoint=127.0.0.1:%s &"%(port))           
+                print((i+1),(path[i].split('/')[-1]),port)
+                os.system("snmpsimd.py --v3-engine-id=010203040505060880 --v3-user=qxf2 --data-dir=%s --agent-udpv4-endpoint=127.0.0.1:%s --logging-method=file:./data/snmp_logs.txt:10m --log-level=debug & "%(path[i],port))
             except OSError:
                 raise ValueError("error in running the snmp device 'snmpsimd.py' command")
-        
+
         ip_list = [faker.ipv4() for port in port_list]
         #self.update_iptables(port_list,ip_list)
         return ip_list
-    
-    
-            
-    
+
+
+
+
 
 
 
 if __name__ == "__main__":
     devices = simulate_snmp_devices()
-    args = devices.parse_args()    
+    args = devices.parse_args()
     if args.Print is True:
         devices.available_templates(args.Print)
     elif args.devices is not False:
         template_path=devices.find_dev_template(args.devices)
-    devices.create_snmp(template_path)
+        if template_path is not None:
+            devices.create_snmp(template_path)
+
     
     
     
